38a4f8c8dd51c7151a4f76750d01cb6c10078fba
JorgeAguilar24 <70876154+JorgeAguilar24@users.noreply.github.com>
2020-09-11 23:43:50 -0700

    Update StudentDesks.java
    completed StudentDesks

--- src/main/java/StudentDesks.java
+++ src/main/java/StudentDesks.java
@@ -6,13 +6,19 @@ class StudentDesks {
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
 
-        //Reads ints from user
-        
-
+        //Reads ints from user and initializes vars
+        int students1 = scanner.nextInt();
+        int students2 = scanner.nextInt();
+        int students3 = scanner.nextInt();
+        int totalStu;
+        int totalDesks;
         /*
-         *  your code goes here
+         * totalDesks gets the values from studentsX divided by 2 so we know how many students are in that class and the we get the remainder of the same class,
+         * so if there is an odd number of students, 1 gets added on. This equation makes sure that theres is enough desks even if there is one extra student.
          */
-
+         
+          totalDesks = ((students1 / 2) + (students1 % 2)) + ((students2 / 2) + (students2 % 2)) + ((students3 / 2) + (students3 % 2));
+          System.out.print(totalDesks);
 
         // closing the scanner object
         scanner.close();
