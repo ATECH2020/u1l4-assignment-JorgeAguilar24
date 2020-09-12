ae2c36a671312b0928d3e9234ba01f55a65072d5
JorgeAguilar24 <70876154+JorgeAguilar24@users.noreply.github.com>
2020-09-11 23:38:10 -0700

    Update AnalogClock.java

--- src/main/java/AnalogClock.java
+++ src/main/java/AnalogClock.java
@@ -6,11 +6,14 @@ class AnalogClock {
         //Creates a new scanner for user input
         Scanner scanner = new Scanner(System.in);
 
-        //Reads ints from user
+        //Reads ints from user and initialize vars
         int hourHand = scanner.nextInt();
         int minuteHand;
         /*
-         *  your code goes here
+         *  the minuteHand equation gets the angle of the hourhand inputted by the user and gets the remainder of it once divided by 30. 
+         We divide it by 30 because that is the amount of degrees from midnight to 1, 
+         and the remainder of it will give us how many the degrees hourHand has moved from the current hour.
+         We multiply by 12 to see how far the minuteHand has moved on the clock.
          */
             minuteHand = (hourHand % 30) * 12;
             System.out.print(minuteHand);
